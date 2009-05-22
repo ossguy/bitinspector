ossguy/bitinspector

commit 438829e7623b42ed18e45801026f0097baf475b4

add labels for input and tracks in display mode
The input and tracks fields now have labels in display mode, indicating
what the fields represent.  A grid layout was added to contain the labels
and fields, which keeps the alignment consistent.

bitprocessor.cpp
@@ -32,6 +32,10 @@ BitProcessor::BitProcessor(QWidget* parent)
 	example_bits = new QLabel;
 	input = new QTextEdit;
 	inspect = new QPushButton(tr("&Inspect"));
+	label_input = new QLabel;
+	label_t1 = new QLabel;
+	label_t2 = new QLabel;
+	label_t3 = new QLabel;
 	out_t1 = new QLineEdit;
 	out_t2 = new QLineEdit;
 	out_t3 = new QLineEdit;
@@ -58,6 +62,12 @@ BitProcessor::BitProcessor(QWidget* parent)
 	// this keeps focus on the input textbox:
 	example_bits->setFocusPolicy(Qt::NoFocus);
 
+	// configure the input and track labels
+	label_input->setText(tr("Input"));
+	label_t1->setText(tr("Track 1"));
+	label_t2->setText(tr("Track 2"));
+	label_t3->setText(tr("Track 3"));
+
 	// configure the fields table
 	fields->setRowCount(0);
 	fields->setColumnCount(3);
@@ -65,24 +75,23 @@ BitProcessor::BitProcessor(QWidget* parent)
 		<< QString(tr("Field name")) << QString(tr("Track"))
 		<< QString(tr("Value on card")));
 
-	// hide widgets for output mode
-	out_t1->hide();
-	out_t2->hide();
-	out_t3->hide();
-	fields->hide();
-	another->hide();
-	show_input->hide();
-	hide_input->hide();
-
 	// arrange input and output widgets in layout
 	QVBoxLayout* layout = new QVBoxLayout;
 	layout->addWidget(instructions);
 	layout->addWidget(example_bits);
-	layout->addWidget(input);
 	layout->addWidget(inspect);
-	layout->addWidget(out_t1);
-	layout->addWidget(out_t2);
-	layout->addWidget(out_t3);
+
+	QGridLayout* grid = new QGridLayout;
+	grid->addWidget(label_input, 0, 0, Qt::AlignTop);
+	grid->addWidget(label_t1, 1, 0);
+	grid->addWidget(label_t2, 2, 0);
+	grid->addWidget(label_t3, 3, 0);
+	grid->addWidget(input, 0, 1);
+	grid->addWidget(out_t1, 1, 1);
+	grid->addWidget(out_t2, 2, 1);
+	grid->addWidget(out_t3, 3, 1);
+	layout->addLayout(grid);
+
 	layout->addWidget(fields);
 
 	QHBoxLayout* out_buttons = new QHBoxLayout;
@@ -92,6 +101,19 @@ BitProcessor::BitProcessor(QWidget* parent)
 	layout->addLayout(out_buttons);
 
 	setLayout(layout);
+
+	// hide widgets for output mode
+	label_input->hide();
+	label_t1->hide();
+	label_t2->hide();
+	label_t3->hide();
+	out_t1->hide();
+	out_t2->hide();
+	out_t3->hide();
+	fields->hide();
+	show_input->hide();
+	hide_input->hide();
+	another->hide();
 
 
 	setWindowTitle(tr("Bit Inspector"));
@@ -104,6 +126,9 @@ BitProcessor::BitProcessor(QWidget* parent)
 	connect(inspect, SIGNAL(released()), example_bits, SLOT(hide()));
 	connect(inspect, SIGNAL(released()), input, SLOT(hide()));
 	connect(inspect, SIGNAL(released()), inspect, SLOT(hide()));
+	connect(inspect, SIGNAL(released()), label_t1, SLOT(show()));
+	connect(inspect, SIGNAL(released()), label_t2, SLOT(show()));
+	connect(inspect, SIGNAL(released()), label_t3, SLOT(show()));
 	connect(inspect, SIGNAL(released()), out_t1, SLOT(show()));
 	connect(inspect, SIGNAL(released()), out_t2, SLOT(show()));
 	connect(inspect, SIGNAL(released()), out_t3, SLOT(show()));
@@ -117,6 +142,10 @@ BitProcessor::BitProcessor(QWidget* parent)
 	connect(another, SIGNAL(released()), input, SLOT(show()));
 	connect(another, SIGNAL(released()), input, SLOT(selectAll()));
 	connect(another, SIGNAL(released()), inspect, SLOT(show()));
+	connect(another, SIGNAL(released()), label_input, SLOT(hide()));
+	connect(another, SIGNAL(released()), label_t1, SLOT(hide()));
+	connect(another, SIGNAL(released()), label_t2, SLOT(hide()));
+	connect(another, SIGNAL(released()), label_t3, SLOT(hide()));
 	connect(another, SIGNAL(released()), out_t1, SLOT(hide()));
 	connect(another, SIGNAL(released()), out_t2, SLOT(hide()));
 	connect(another, SIGNAL(released()), out_t3, SLOT(hide()));
@@ -126,11 +155,13 @@ BitProcessor::BitProcessor(QWidget* parent)
 	connect(another, SIGNAL(released()), another, SLOT(hide()));
 
 	// on "Show Input", show the input textbox and replace with "Hide Input"
+	connect(show_input, SIGNAL(released()), label_input, SLOT(show()));
 	connect(show_input, SIGNAL(released()), input, SLOT(show()));
 	connect(show_input, SIGNAL(released()), show_input, SLOT(hide()));
 	connect(show_input, SIGNAL(released()), hide_input, SLOT(show()));
 
 	// on "Hide Input", hide the input textbox and replace with "Show Input"
+	connect(hide_input, SIGNAL(released()), label_input, SLOT(hide()));
 	connect(hide_input, SIGNAL(released()), input, SLOT(hide()));
 	connect(hide_input, SIGNAL(released()), hide_input, SLOT(hide()));
 	connect(hide_input, SIGNAL(released()), show_input, SLOT(show()));

bitprocessor.h
@@ -44,6 +44,10 @@ private:
 	QLabel* example_bits;
 	QTextEdit* input;
 	QPushButton* inspect;
+	QLabel* label_input;
+	QLabel* label_t1;
+	QLabel* label_t2;
+	QLabel* label_t3;
 	QLineEdit* out_t1;
 	QLineEdit* out_t2;
 	QLineEdit* out_t3;

commit f5b45d52c63a4e9f8baf1aec745128c3b839cbc8

fix location of "Inspect" button
Commit 438829e introduced a regression that moved the "Inspect" button
above the input text box.  This commit fixes that regression by moving the
"Inspect" button below the grid layout, which now contains the input text
box.

bitprocessor.cpp
@@ -79,7 +79,6 @@ BitProcessor::BitProcessor(QWidget* parent)
 	QVBoxLayout* layout = new QVBoxLayout;
 	layout->addWidget(instructions);
 	layout->addWidget(example_bits);
-	layout->addWidget(inspect);
 
 	QGridLayout* grid = new QGridLayout;
 	grid->addWidget(label_input, 0, 0, Qt::AlignTop);
@@ -92,6 +91,7 @@ BitProcessor::BitProcessor(QWidget* parent)
 	grid->addWidget(out_t3, 3, 1);
 	layout->addLayout(grid);
 
+	layout->addWidget(inspect); // at the bottom in input mode
 	layout->addWidget(fields);
 
 	QHBoxLayout* out_buttons = new QHBoxLayout;